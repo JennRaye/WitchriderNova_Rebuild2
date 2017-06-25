a1e5ce7254289be793526323b78df16fc84c1c38
Jennifer Raye <jtbont@live.com>
2017-06-24 23:29:19 -0600

    Control Updates
    Generally improving the feel of the physics... still a bit to go but i might be on to something! <3

--- Assets/WitchScript.cs
+++ Assets/WitchScript.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class WitchScript : MonoBehaviour {
 	public float speed;
+	public float Acceleration;
+	public float SlowdownAcceleration;
+	public float topSpeed;
+
+
+
 	public float rotSpeed;
+	public float rotAcceleration;
+	public float rotSlowdownAcceleration;
+	public float rotTopSpeed;
+
 	public float jumpSpeed;
 	public float gravity;
 	private Vector3 moveDirection = Vector3.zero;
 	public CharacterController controller;
 	public Transform thisTrans;
 
+	public int Momentum;
+	public Vector3 MomentumMoveDirectionSave;
 
+
+	public Vector3 VelocitySave;
 	void Start() {
 		controller = GetComponent<CharacterController>();
 	}
@@ -26,13 +40,63 @@ public class WitchScript : MonoBehaviour {
 
 
 
+		if (Input.GetButton ("Accelerate")) {
+			if (speed < topSpeed)
+				speed += Acceleration * Time.deltaTime;
+			else
+				speed = topSpeed;
+		} else {
+			if (speed > 0)
+				speed -= SlowdownAcceleration * Time.deltaTime;
+			else
+				speed = 0;
+		}
 
 
-		// moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		if (Input.GetButton("Accelerate")) moveDirection = new Vector3(moveDirection.x, moveDirection.y, speed * Time.deltaTime);
+		moveDirection = new Vector3(moveDirection.x, moveDirection.y, speed * Time.deltaTime);
+
 
 
 		moveDirection = transform.TransformDirection (moveDirection);
+
+//		if (Input.GetAxis ("Horizontal") != 0){
+//			if (Momentum == 0) {
+//				MomentumMoveDirectionSave = moveDirection;
+//				Momentum = 1;
+//			}
+//		}
+//
+//
+//		if (Momentum == 1) {
+//
+//			moveDirection = new Vector3(moveDirection.x + MomentumMoveDirectionSave.x, moveDirection.y + MomentumMoveDirectionSave.y, speed * Time.deltaTime);
+//			if (Input.GetAxis ("Horizontal") == 0)
+//				Momentum = 0;
+//		}
+
+
+		if (Input.GetAxis ("Horizontal") != 0){
+			if (Momentum == 0) {
+				VelocitySave = controller.velocity;
+				Momentum = 1;
+			}
+		}
+
+		if (Momentum == 1) {
+			VelocitySave = new Vector3 (Mathf.Lerp (VelocitySave.x, 0, Time.deltaTime * 0.4f), VelocitySave.y, Mathf.Lerp (VelocitySave.z, 0, Time.deltaTime * 0.4f));
+			moveDirection = new Vector3(VelocitySave.x, moveDirection.y, VelocitySave.z);
+			if (Input.GetAxis ("Horizontal") == 0)
+				Momentum = 0;
+		}
+
+
+
+
+		Debug.Log (controller.velocity);
+
+
+
+
 
 		thisTrans.Rotate (0, Input.GetAxis ("Horizontal") * rotSpeed * Time.deltaTime, 0);
 
@@ -46,7 +110,7 @@ public class WitchScript : MonoBehaviour {
 
 
 		controller.Move(moveDirection * Time.deltaTime);
-		
+
 	}
 
 }
