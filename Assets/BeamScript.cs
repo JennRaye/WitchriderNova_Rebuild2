dd9b69df8e172b2f09140d1bfde4e9e87086aa26
Jennifer Raye <jtbont@live.com>
2017-07-12 10:42:09 -0600

    Beam Code and Some Geometry simplifying

---
+++ Assets/BeamScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamScript : MonoBehaviour {
+	public Vector2 MinAxis;
+	public Vector2 MaxAxis;
+	public Transform thisTrans;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+	void OnTriggerEnter(Collider hit){
+		if (hit.gameObject.tag == "WitchPlayer") {
+			thisTrans.position = new Vector3 (Random.Range (MinAxis.x, MaxAxis.x), 0, Random.Range (MinAxis.y, MaxAxis.y));
+		}
+	}
+}
