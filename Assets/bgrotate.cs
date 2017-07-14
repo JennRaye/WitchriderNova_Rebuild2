8311e17376765a1689ac592b7256cdf9665fbb5e
Emmeline Bonne <emmeline.bonne@gmail.com>
2017-07-14 13:21:47 -0600

    hi
    character select startin up

---
+++ Assets/bgrotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgrotate : MonoBehaviour {
+
+	public Transform transform;
+	public float rotationspeed;
+		
+	// Use this for initialization
+	void Start () {
+		transform = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * rotationspeed * Time.deltaTime);
+	}
+}
