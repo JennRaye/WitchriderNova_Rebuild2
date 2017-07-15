bd418caeb36ece56f1fcae93d6c12e16dd433c93
Emmeline Bonne <emmeline.bonne@gmail.com>
2017-07-14 21:25:29 -0600

    character select
    the beginning of the menuuuu

---
+++ Assets/characterselect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterselect : MonoBehaviour {
+
+	public float currentcharacter;
+	public float charactertotal;
+	private bool pressed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		currentcharacter = Mathf.Abs(currentcharacter % charactertotal);
+
+		if (Input.GetAxis ("Horizontal") == 0 && pressed == true){
+			pressed = false;
+		}
+
+		if (Input.GetAxis ("Horizontal") > 0 && pressed == false) {
+			pressed = true;
+			currentcharacter += 1;
+		}
+	
+		if (Input.GetAxis ("Horizontal") < 0 && pressed == false) {
+			pressed = true;
+			currentcharacter -= 1;
+		}
+	}
+}
---
+++ Assets/characterviewer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterviewer : MonoBehaviour {
+
+    public GameObject Child;
+	public characterselect characterselect;
+	public float characterindex;
+	public MeshRenderer MeshRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+		MeshRenderer = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		characterselect = GetComponentInParent<characterselect> ();
+
+		if (characterselect.currentcharacter == characterindex) {
+			MeshRenderer.enabled = true;
+			Child.SetActive (true);
+		}
+
+		if (characterselect.currentcharacter != characterindex) {
+			MeshRenderer.enabled = false; 
+			Child.SetActive (false);
+		}
+
+	}
+}
